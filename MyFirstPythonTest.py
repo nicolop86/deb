c571ec29a961600998469e6ba9f0bed097427f5a
Nicolò Politi <nicolopoliti86@gmail.com>
2019-05-22 15:25:10 +0200

    Some more python

---
+++ MyFirstPythonTest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+print "This is my first python test!"
+# this is a comment
+
+spam = 1  # and this is the second comment
+          # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+print text
+sum=2+2
+div=6.0/5
+carry=6.0%5
+print sum
+print div
+print carry
