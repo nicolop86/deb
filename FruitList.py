23f7fff8cea6677ba002792e05c9e65014995be7
Nicolò Politi <nicolopoliti86@gmail.com>
2019-07-04 12:44:50 +0200

    Some more exercises on lists and queues

---
+++ FruitList.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.5
+fruits=['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+fruits.count('apple')
+fruits.count('tangerine')
+fruits.index('banana')
+fruits.index('banana', 4)
+fruits.reverse()
+print(fruits)
+fruits.append('grape')
+print(fruits)
+fruits.sort()
+print(fruits)
+print(fruits.pop())
+
--- Queues.py
+++ Queues.py
@@ -1,14 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python3.5
 from collections import deque
 queue = deque(["Eric", "John", "Michael"])
-queue.append("Terry")           # Terry arrives
-queue.append("Graham")          # Graham arrives
-queue.popleft()                 # The first to arrive now leaves
-
-queue.popleft()                 # The second to arrive now leaves
-
-print(queue)                           # Remaining queue in order of arrival
-
-combs=[(x, y) for x in [1,2,3] for y in [10,3,1,4] if x != y]
-print(combs)
+queue.append("Terry")
+print(queue)
+queue.append("Graham")
+print(queue)
+queue.popleft()
+print(queue)
+queue.popleft()
+print(queue)
 
