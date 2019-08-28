9e988f61a1cf446c4d0a31e25b6640c3cca78757
Nicolò Politi <nicolopoliti86@gmail.com>
2019-08-28 17:29:22 +0200

    Something about definitions

--- dictionaries.py
+++ dictionaries.py
@@ -8,3 +8,25 @@ print(sorted(tel.keys()))
 newDict=dict([('one', 1), ('two', 2), ('three', 3)])
 print(newDict)
 
+print('Looping over tel')
+for k, v in tel.items():
+	print(k, v)
+
+print('Looping over a sequence')
+for i, v in enumerate(['tic', 'tac', 'toe']):
+	print(i, v)
+
+print('Use of zip function')
+teams = ['Milan', 'Sporting Lisbona', 'Ajax']
+definitions = ['AC', 'FC', 'FC']
+for q, a in zip(definitions, teams):
+	print('My favourite team is {0} {1}'.format(q, a))
+
+a = (1, 2, 3)
+b = (1, 2, 5)
+
+if (a < b): 
+	print('True')
+else:
+	print('False')
+
